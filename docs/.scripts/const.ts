fd72b3ca13d92bca49e298c9fee7e6ff036f6afb
ateals <ateals@icloud.com>
2024-07-03 01:09:38 +0900

    fix: tsx and script

--- docs/.scripts/const.ts
+++ docs/.scripts/const.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 
 export const PROJECT_ROOT = path.resolve(__dirname, '..', '..');
-export const PACKAGES_ROOT = path.resolve(PROJECT_ROOT, 'packages');
+export const PACKAGES_ROOT = PROJECT_ROOT;
 export const DOCUSAURUS_ROOT = path.resolve(PROJECT_ROOT, 'docs');
 export const DOCS_ROOT = path.resolve(DOCUSAURUS_ROOT, 'docs');
